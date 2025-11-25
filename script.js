d041f26bede138453185fac7c774adb1c0892807
lonepianist <147939850+lonepianist@users.noreply.github.com>
2025-11-25 19:33:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 function downloadPDF() {
     window.print();
 }
+
+// STAR RATING LOGIC
+document.querySelectorAll(".stars").forEach(star => {
+    const rating = star.getAttribute("data-rating");
+    const percent = (rating / 5) * 100;
+    star.style.setProperty("--percent", percent + "%");
+});
