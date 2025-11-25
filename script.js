b3a1fc6c87696b501cd8339109a373db9ba806f5
lonepianist <147939850+lonepianist@users.noreply.github.com>
2025-11-25 19:40:37 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ function downloadPDF() {
     window.print();
 }
 
-// STAR RATING LOGIC
-document.querySelectorAll(".stars").forEach(star => {
-    const rating = star.getAttribute("data-rating");
-    const percent = (rating / 5) * 100;
-    star.style.setProperty("--percent", percent + "%");
+// Convert rating (1-5) to bar width
+document.querySelectorAll(".fill").forEach(bar => {
+    const rating = bar.getAttribute("data-rating");
+    const width = (rating / 5) * 100; // rating to %
+    bar.style.width = width + "%";
 });
